cc60fdc2678eaaf02b2fac19e997cb306933fd09
ROCKY <riazulrocky@gmail.com>
2024-08-19 23:12:32 +0600

    bottom nav bar

---
+++ lib/pages/market.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class market extends StatelessWidget {
+  const market({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("market"));
+  }
+}
