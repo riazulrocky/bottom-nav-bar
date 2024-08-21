cf8e7fe64b46655c8899337f9235ce203e2b3527
ROCKY <riazulrocky@gmail.com>
2024-08-21 20:39:37 +0600

    bottom nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,51 @@
+import 'package:bottom_nav_bar/pages/friends.dart';
 import 'package:bottom_nav_bar/pages/home.dart';
+import 'package:bottom_nav_bar/pages/market.dart';
+import 'package:bottom_nav_bar/pages/profile.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
+
+  List<Widget> pages = [home(), friends(), market(), profile()];
+
+  int index = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Bottom Navigation Bar',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 2,
+            items: [
+          BottomNavigationBarItem(
+            backgroundColor: Colors.red,
+            label: "Home",
+              icon: Icon(Icons.home)
+          ),
+          BottomNavigationBarItem(
+              backgroundColor: Colors.red,
+              label: "Friends",
+              icon: Icon(Icons.supervised_user_circle)
+          ),
+          BottomNavigationBarItem(
+              backgroundColor: Colors.red,
+              label: "Market",
+              icon: Icon(Icons.shopping_cart)
+          ),
+          BottomNavigationBarItem(
+              backgroundColor: Colors.red,
+              label: "Profile",
+              icon: Icon(Icons.account_box_rounded)
+          ),
+        ]
+        ),
       ),
-      home: home(),
     );
   }
 }
