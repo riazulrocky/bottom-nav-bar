3fbda8a56c7c2e966b9acb62515bfb62af1c4923
ROCKY <riazulrocky@gmail.com>
2024-08-22 20:08:22 +0600

    bottom nav bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,6 +5,6 @@ class home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("HOME"));
+    return const Center(child: Text("Welcome"));
   }
 }
